a97ce869868e598abf26cc9ea7b76193e8977211
caggiaC <ctcaggia.chris@gmail.com>
2025-02-28 20:29:33 -0500

    Add project files.

---
+++ Calencadabra/Models/Note.cs
@@ -0,0 +1,26 @@
+﻿namespace Calencadabra.Models
+{
+    public class Note
+    {
+        private string _text = string.Empty;
+
+        public DateTime DateCreated { get; } = DateTime.Now;
+        public DateTime DateLastModified { get; set; } = DateTime.Now;
+        public string Text{ get { return _text; } }
+
+        public bool Set(string text_in)
+        {
+            try
+            {
+                _text = text_in;
+                DateLastModified = DateTime.Now;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }   
+            return true;
+        }
+    }
+}
